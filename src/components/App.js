6f2675992c4085dcebcc1590d2831e6256b99757
Maria Luisa Lufrano <marialuisa.lufrano@icloud.com>
2022-10-04 12:53:01 +0200

    Inserted add phrase and title functionality

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,14 @@ import api from '../services/api';
 import Filters from './Filters';
 import { useState, useEffect } from 'react';
 import Posts from './Posts';
+import NewPhrase from './NewPhrase';
 
 function App() {
 
 const [dataApi, setDataApi] = useState([]);
 const [filterByTitle, setFilterByTitle] = useState("");
+const [newPhrase, setNewPhrase] = useState("");
+const [newTitle, setNewTitle] = useState("");
 
 useEffect(() => {
   api().then((dataFromApi) => {
@@ -19,12 +22,42 @@ const handleFilterByTitle = (value) => {
   setFilterByTitle(value)
 }
 
+const saveNewPhrase = (value) => {
+  setNewPhrase(value);
+}
+
+const saveNewTitle = (value) => {
+  setNewTitle(value);
+}
+
+const addNewPhraseandTitle = (ev) => {
+  ev.preventDefault();
+  if(newPhrase === "" || newTitle === "") {
+    alert('You have to fill all the inputs');
+    return false
+  } else {
+    const newList = {body: newPhrase, title: newTitle};
+  setDataApi([...dataApi, newList])
+  setNewPhrase("");
+  setNewTitle("");
+  }
+}
+
+
   return (
     <div className="App">
       <h1 className='header-title'>Post</h1>
       <Filters
       handleFilterByTitle={handleFilterByTitle}
       filterByTitle={filterByTitle}/>
+      <div className='wrapper-newPhrase'>
+      <NewPhrase
+      saveNewTitle={saveNewTitle}
+      newTitle={newTitle}
+      newPhrase={newPhrase}
+      saveNewPhrase={saveNewPhrase}
+      addNewPhraseandTitle={addNewPhraseandTitle}/>
+      </div>
       <ul className='wrapper-list'>
         <Posts
         dataApi={dataApi}
---
+++ src/components/NewPhrase.js
@@ -0,0 +1,35 @@
+const NewPhrase = (props) => {
+
+    const saveNewPhrase = (ev) => {
+        props.saveNewPhrase(ev.target.value);
+      }
+      
+      const saveNewTitle = (ev) => {
+        props.saveNewTitle(ev.target.value);
+      }
+
+    return (<>
+    <form className="formName">
+    <label htmlFor='labelName' className="labelName">
+      Title:
+      <input type="text" 
+             name="name"
+             id='text'
+             className="inputName"
+             value={props.newTitle}
+             onChange={saveNewTitle}/>
+    </label>
+    <label htmlFor='labelName' className="labelName">
+      Phrase:
+      <input type="text" 
+             name="name"
+             className="inputName"
+             value={props.NewPhrase}
+             onChange={saveNewPhrase}/>
+    </label>
+  </form>
+  <button onClick={props.addNewPhraseandTitle} className='addButton'>Add</button>
+  </>)
+}
+
+export default NewPhrase;
